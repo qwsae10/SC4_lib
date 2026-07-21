2cd953a788995da6c8b236ce49d7b26d88a4793b
Priya Shukla <teslapriya.com>
2026-07-21 13:15:54 -0500

    add

--- src/scintkit/sc4_reading/sf_concat.py
+++ src/scintkit/sc4_reading/sf_concat.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import pandas as pd
 
 # Folder containing parquet files
-data_dir = Path("/home/dal674840/scratch/20241010/20241010_parquet")
+data_dir = Path("/home/dal674840/scratch/20240629/20240629_parquet")
 
 # Read all files for the station
 files = sorted(data_dir.glob("*19.2235E_34.4244S*_lvl0.pq"))
@@ -30,7 +30,7 @@ day_df = day_df.sort_values("datetime").reset_index(drop=True)
 # Optional: remove duplicate rows
 day_df = day_df.drop_duplicates()
 
-output_file = data_dir / "scintpi3_20241010_day_19.2235E_34.4244S_lvl0.pq"
+output_file = data_dir / "scintpi3_20240629_day_19.2235E_34.4244S_lvl0.pq"
 
 day_df.to_parquet(
     output_file,
