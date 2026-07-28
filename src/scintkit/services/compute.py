335734b3ad4ba99c6a3d876eeee50536773d1d0d
qwsae10 <isaacgarywright@gmail.com>
2026-07-28 11:54:59 -0500

    fix files

--- src/scintkit/services/compute.py
+++ src/scintkit/services/compute.py
@@ -296,8 +296,6 @@ def _add_quality_flags(products, fs):
     return products.drop(columns=internal_columns, errors="ignore")
 
 
-def add_products(df,verbose=False):
-
 def add_products(df,verbose=False,fs=None):
     """
     This function takes a full-rate dataframe (fs=20 or 10 Hz) at and computes various products:
